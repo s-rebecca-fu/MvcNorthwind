615b7a87f5e6bbd2b11b58b0dcf459defbba9e46
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-01-26 14:00:30 -0500

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_DB_lec.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_DB_lec.Startup))]
+namespace MVC_DB_lec
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
